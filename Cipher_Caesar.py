fb4c0aaa2a6550439f51db86f3e3352ad77f53af
divya42536 <divya42536@gmail.com>
2025-03-10 11:44:45 -0700

    Create Cipher_Caesar.py

---
+++ Cipher_Caesar.py
@@ -0,0 +1,35 @@
+# def greet():
+#     print("Hello Coders")
+#     print("welcome to python world!")
+#     print("All the very best")
+
+# greet()
+
+# def greet_with_name(name):
+
+#     print(f"hello {name}" )
+#     print(f"Hello Coders {name}")
+#     print("welcome to python world!")
+#     print("All the very best")
+
+# greet_with_name("Divya")
+
+# def life_in_weeks(age):
+    
+#     # age =  int(input("Enter your age :{age}"))
+    
+#     weeks_left = (90 - age )*52
+
+#     print(f"You have {weeks_left}  weeks left.")
+
+# life_in_weeks(12)
+
+# functions with inputs
+
+def greet_with(name,location):
+    # name = input ("enter the name: {name}")
+    # location = input("enter the location : {location}")
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(location ="Seattle", name="Divya")
